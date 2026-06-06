3b32cc7511ace7901d2a8f8e68487daf0aebb8e2
Jsprrr69 <jasperallen07@gmail.com>
2026-06-06 12:16:42 +0800

    Update manual.py

--- manual.py
+++ manual.py
@@ -552,6 +552,7 @@ page = st.sidebar.radio(
         "🎥 Demonstration",
         "❓ FAQ",
         "👨‍💻 Developers"
+        "🎮 Fire Safety Quiz"
     ]
 )
 
@@ -1478,6 +1479,25 @@ elif page == "👨‍💻 Developers":
 
     with Automatic Breaker Isolation and SMS Notification
     """)
+elif page == "🎮 Fire Safety Quiz":
+
+    st.title("🎮 Fire Safety Quiz")
+
+    question = st.radio(
+        "What condition does burning paper belong to?",
+        [
+            "Non-Fire",
+            "Potential Fire",
+            "Fire"
+        ]
+    )
+
+    if st.button("Submit"):
+
+        if question == "Fire":
+            st.success("✅ Correct!")
+        else:
+            st.error("❌ Incorrect. Burning paper is classified as Fire.")
 
 # FOOTER #
 st.markdown("""
