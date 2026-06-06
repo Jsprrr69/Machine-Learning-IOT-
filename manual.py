c5ac90ab7626836faefc39f48f3844572e68fc8e
Jsprrr69 <jasperallen07@gmail.com>
2026-06-06 21:59:36 +0800

    Update manual.py

--- manual.py
+++ manual.py
@@ -900,7 +900,7 @@ st.markdown("""
     ### 🚦 Step 4 — Classification Result
     """)
 
-    col1, col2, col3 = st.columns(3)
+col1, col2, col3 = st.columns(3)
 
     with col1:
         st.success("""
