affc1393815d0f1e8aa6d12cd935818af8f6a55b
Jsprrr69 <jasperallen07@gmail.com>
2026-06-07 15:49:48 +0800

    Update manual.py

--- manual.py
+++ manual.py
@@ -688,6 +688,14 @@ Group 5
 # ==========================================================
 
 if page == "🏠 Home":
+
+    col1, col2, col3 = st.columns([1,1,1])
+
+    with col2:
+        st.image(
+            "assets/logo.png",
+            width=300
+        )
     
     st.markdown(
         """
