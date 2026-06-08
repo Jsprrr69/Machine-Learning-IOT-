eef03f3e6328244296d1fe3ce540be4b73a61cee
Jsprrr69 <jasperallen07@gmail.com>
2026-06-08 18:41:10 +0800

    Update manual.py

--- manual.py
+++ manual.py
@@ -728,7 +728,7 @@ if page == "🏠 Home":
     IoT-Based Fire Detection
     and Classification System
     with Automatic Breaker Isolation
-    and SMS Notification and Jzenarah Kalibu
+    and SMS Notification
     </h1>
 
     <br>
